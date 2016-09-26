c0f6e2af7739da5bf05992a498701d3bf79ca46d
Ryo Kanazawa <ryoau9@gmail.com>
2016-09-27 02:58:08 +0900

    typo

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,8 +1,4 @@
 #! /bin/bash
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.gvimrc ~/.gvimrc
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/Git/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/Git/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/Git/dotfiles/.inputrc ~/.inputrc
