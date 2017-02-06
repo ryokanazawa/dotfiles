c6532b813975484a3ef686ccd9637766bcdc19a2
Ryo Kanazawa <ryoau9@gmail.com>
2017-02-07 01:20:40 +0900

    Add .irbrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.inputrc ~/.inputrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.irbrc ~/.irbrc
