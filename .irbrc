6329a35c551763e2384af27c1a6001881d648241
Ryo Kanazawa <ryoau9@gmail.com>
2019-10-11 18:53:40 +0900

    いらない設定を削除

--- .irbrc
+++
@@ -1,7 +0,0 @@
-#require 'irb/completion'
-require 'pp'
-require 'rubygems'
-require 'wirble'
-
-Wirble.init
-Wirble.colorize
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,12 +1,6 @@
 #! /bin/bash
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.inputrc ~/.inputrc
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.irbrc ~/.irbrc
-ln -s ~/dotfiles/.pryrc ~/.pryrc
 ln -s ~/dotfiles/.rubocop.yml ~/.rubocop.yml
-ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.git-prompt.sh ~/.git-prompt.sh
-ln -s ~/dotfiles/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -s ~/dotfiles/config.fish ~/.config/fish/config.fish
