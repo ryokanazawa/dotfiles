20517b204a8f1dfba29b75232cc873a0ed7db373
Ryo Kanazawa <ryoau9@gmail.com>
2019-11-23 00:55:10 +0900

    fix pyenv

--- config.fish
+++ config.fish
@@ -1,9 +1,14 @@
 set -x PATH $HOME/.anyenv/bin $PATH
+
 set -x GOENV_ROOT $HOME/.anyenv/envs/goenv
 set -x PATH $HOME/.anyenv/envs/goenv/bin $PATH
 set -x PATH $HOME/.anyenv/envs/goenv/shims $PATH
+
 set -x RBENV_ROOT $HOME/.anyenv/envs/rbenv
 set -x PATH $HOME/.anyenv/envs/rbenv/bin $PATH
 set -x PATH $HOME/.anyenv/envs/rbenv/shims $PATH
+
+set -x PYENV_ROOT $HOME/.anyenv/envs/pyenv
+set -x PATH $PYENV_ROOT/bin $PATH
+
 set -x PATH /usr/local/opt/imagemagick@6/bin/ $PATH
-set -x PATH $HOME/.anyenv/envs/pyenv/bin $PATH
