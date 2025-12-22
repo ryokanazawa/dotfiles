89f69fcc552927907b3f7246cfc9b01422497209
Ryo Kanazawa <ryoau9@gmail.com>
2025-12-22 15:26:43 +0900

    VSCode設定ファイルをvscodeディレクトリに整理

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.rubocop.yml ~/.rubocop.yml
-ln -s ~/dotfiles/.git-prompt.sh ~/.git-prompt.sh
-ln -s ~/dotfiles/.inputrc ~/.inputrc
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -s ~/dotfiles/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-ln -s ~/dotfiles/karabiner.json ~/.config/karabiner/karabiner.json
+ln -s ~/Developer/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/Developer/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/Developer/dotfiles/.rubocop.yml ~/.rubocop.yml
+ln -s ~/Developer/dotfiles/.git-prompt.sh ~/.git-prompt.sh
+ln -s ~/Developer/dotfiles/.inputrc ~/.inputrc
+ln -s ~/Developer/dotfiles/.vimrc ~/.vimrc
+ln -s ~/Developer/dotfiles/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/Developer/dotfiles/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/Developer/dotfiles/karabiner.json ~/.config/karabiner/karabiner.json
