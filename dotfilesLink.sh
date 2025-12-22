a5734a0aa1c3f52c6f39a010addd514c01f138bf
Ryo Kanazawa <ryoau9@gmail.com>
2025-12-23 00:01:27 +0900

    VSCode設定のパスを修正し、Cursor設定のシンボリックリンクを追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,6 +5,8 @@ ln -s ~/Developer/dotfiles/.rubocop.yml ~/.rubocop.yml
 ln -s ~/Developer/dotfiles/.git-prompt.sh ~/.git-prompt.sh
 ln -s ~/Developer/dotfiles/.inputrc ~/.inputrc
 ln -s ~/Developer/dotfiles/.vimrc ~/.vimrc
-ln -s ~/Developer/dotfiles/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -s ~/Developer/dotfiles/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/Developer/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/Developer/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/Developer/dotfiles/cursor/settings.json ~/Library/Application\ Support/Cursor/User/settings.json
+ln -s ~/Developer/dotfiles/cursor/keybindings.json ~/Library/Application\ Support/Cursor/User/keybindings.json
 ln -s ~/Developer/dotfiles/karabiner.json ~/.config/karabiner/karabiner.json
